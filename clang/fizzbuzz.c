ccfbdbfa568aba42c1e9bb8c02178dded95dd395
KenyuF <kenyu.fujimoto@icloud.com>
2026-08-06 15:52:48 +0900

    clang and java

---
+++ clang/fizzbuzz.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+  for (int i = 1; i < 101; i++) {
+    bool divByThree = i % 3 == 0;
+    bool divByFive = i % 5 == 0;
+    if (!divByThree && !divByFive) {
+      printf("%d", i);
+    } else {
+      if (divByThree) {
+        printf("Fizz");
+      }
+      if (divByFive) {
+        printf("Buzz");
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
