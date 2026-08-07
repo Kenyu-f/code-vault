0916e8759242a44dc7cdb36a83c072445a5b8221
KenyuF <kenyu.fujimoto@icloud.com>
2026-08-07 22:22:38 +0900

    java

---
+++ java/variable.java
@@ -0,0 +1,9 @@
+public class variable{
+  public static void main(String[] args){
+    int age = 20;
+    int year = 2026;
+    System.out.println(age);
+    System.out.println("This year is " + year);
+  }
+
+}
